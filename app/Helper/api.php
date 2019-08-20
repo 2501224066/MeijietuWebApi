9b0a556925f1261ec730cd49ff46087f624022a9
figurant <2501224066@qq.com>
2019-08-20 17:37:28 +0800

    百度推送

--- app/Helper/api.php
+++ app/Helper/api.php
@@ -124,6 +124,6 @@ function pushToBaidu($goodsNum)
     $url   = $host . $path . "?" . $querys;
 
     $re =  API_CURL($host, $url, $method, $headers, $bodys);
-    \Illuminate\Support\Facades\Log::info('【百度推送】',$goodsNum.$re);
+    \Illuminate\Support\Facades\Log::info('【百度推送】'.$goodsNum,$re);
     return $re;
 }
